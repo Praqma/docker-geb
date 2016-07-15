41cc4c3baa4c57224d27e8010cbe46b5a842abe7
illus0r <zor667@gmail.com>
2016-07-15 17:26:49 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd /home/jenkins/site/
+mkdir _site
 ls -la
 
 # running `jekyll serve` in background
 exec 3< <(jekyll serve &)
 # waiting for server is ready
-sed '/Server running/q' <&3 ; cat <&3 &
+sed '/Sasdfasdfasdferver running/q' <&3 ; cat <&3 &
 echo "Got respond from server, running tests..."
 groovy ./tests/main.groovy

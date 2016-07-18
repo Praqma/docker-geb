125202426a9808e0fe6a233910e12d6130679b84
illus0r <zor667@gmail.com>
2016-07-18 16:51:35 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 cd /home/jenkins/site/
-sudo mkdir _site
-sudo chmod 777 _site/
-ls -la
+sudo mkdir /home/jenkins/_site
+sudo chmod 777 /home/jenkins/_site/ -R
 
 # running `jekyll serve` in background
-exec 3< <(jekyll serve &)
+exec 3< <(jekyll serve --destination "/home/jenkins/_site"  &)
 # waiting for server is ready
 sed '/Server running/q' <&3 ; cat <&3 &
 echo "Got respond from server, running tests..."
